4109f5bb4d60e429b0eb8f65c08c6868440936cf
SHAJ <shakilahmedshaj@gmail.com>
2018-12-30 20:30:52 +0600

    email validation

--- 8_Filter_Validation.php
+++ 8_Filter_Validation.php
@@ -1,11 +1,26 @@
 <?php
 //check posted data
-
+/*
 if (filter_has_var(INPUT_POST,'data')) {
     echo 'Data Found';
 } else {
     echo 'No Data Found!';
 }
+*/
+
+if (filter_has_var(INPUT_POST,'data')) {
+   
+    if (filter_input(INPUT_POST,'data',FILTER_VALIDATE_EMAIL)) {
+        echo 'Email is Valid';
+    }
+    else {
+        echo 'Email is not Valid';
+    }
+
+
+} 
+
+
 
 ?>
 
