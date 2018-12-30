5a6ae1c34178fb6f6ea588b7c6646cbd8b62acf9
SHAJ <shakilahmedshaj@gmail.com>
2018-12-30 14:10:03 +0600

    server info project

---
+++ ServerInfo/index.php
@@ -0,0 +1,54 @@
+<?php include 'serverinfo.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>System Info</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container">
+        <h1>Server & File Info</h1>
+
+        <?php if ($server) : ?>
+            <ul class="list-group">
+                <?php foreach ($server as $key => $value) : ?>
+                    <li class="list-group-item">
+                        <strong><?php echo $key; ?> </strong>
+                        <?php echo $value ?>
+                    </li>
+
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+
+        <h1>Server & File Info</h1>
+
+        <?php if ($client) : ?>
+            <ul class="list-group">
+                <?php foreach ($client as $key => $value) : ?>
+                    <li class="list-group-item">
+                        <strong><?php echo $key; ?> </strong>
+                        <?php echo $value ?>
+                    </li>
+
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+
+
+    </div>
+
+
+
+
+        
+</body>
+</html>
